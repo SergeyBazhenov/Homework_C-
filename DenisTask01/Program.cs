2f8c1d998c55cb5e13da7d26823de3e8de6adeba
SergeyBazhenov <8618@mail.ru>
2022-10-16 18:20:19 +0500

    Исправлены имена переменных в DenisTask01

--- DenisTask01/Program.cs
+++ DenisTask01/Program.cs
@@ -1,19 +1,20 @@
 ﻿// 1. В переменной string есть секретное сообщение, во второй есть пароль. Пользователю программы даётся 3 попытки ввести пароль и увидеть секретное сообщение.
 string secret = "У меня нет от Вас секретов ;)!";
+string correctPassword = "QWERTY";
 int count = 3;
 while (count > 0)
 {
-Console.WriteLine("У Вас есть " + count + " попытки чтоб узать секрет!");
-Console.Write("Введите пароль: ");
-string Pass = Console.ReadLine();
-if (Pass == "QWERTY")
-{
-    Console.WriteLine(secret);
-    break;
-}
-else
-{
-    Console.WriteLine("Пароль не подходит!");
-}
+    Console.WriteLine("У Вас есть " + count + " попытки чтоб узать секрет!");
+    Console.Write("Введите пароль: ");
+    string password = Console.ReadLine();
+    if (password == correctPassword)
+    {
+        Console.WriteLine(secret);
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Пароль не подходит!");
+    }
 count--;
 }
