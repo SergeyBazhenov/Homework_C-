7b8b2e2509215dfcdf70a5eaf50397b39cec76ad
SergeyBazhenov <8618@mail.ru>
2022-10-18 23:05:56 +0500

    Решение задачи Дениса

--- DenisTask004/Program.cs
+++ DenisTask004/Program.cs
@@ -4,33 +4,62 @@ Help – вывести список команд
 SetPassword – Установить пароль
 Exit – выход
 WriteName – вывести имя после ввода пароля */
-string[] help = { "Help – Вывести список команд", "SetName – Установить имя", "SetPassword – Установить пароль", "ResetPassword - Сбросить пароль",
-                "WriteName – Вывести имя после ввода пароля ", "Exit – Выход"};
-string secret = "У меня нет от Вас секретов ;)!";
-string correctPassword = "QWERTY";
+string[] help = { "Help –> Вывести список команд", "SetName –> Установить имя", "SetPassword –> Установить пароль", "ResetPassword –> Сбросить пароль",
+                "ShowPassword -> Показать пароль", "WriteName –> Вывести имя после ввода пароля ", "Exit –> Выход"};
+string name = "";
+string password = "";
 
-int i = 0;
-while (i < help.Length)
+while (1 > 0)
 {
-    Console.WriteLine(help[i]);
-    i++;
-}
-
-
-/*int count = 3;
-while (count > 0)
-{
-    Console.WriteLine("У Вас есть " + count + " попытки чтоб узать секрет! Список доступных комманд введите Help.");
-    Console.Write("Введите пароль: ");
-    string password = Console.ReadLine();
-    if (password == correctPassword)
+    Console.Write("Введите команду (посмотеть список доступных команд \"Help\"): ");
+    string command = Console.ReadLine();
+    if (command.ToLower() == "help")
     {
-        Console.WriteLine(secret);
+        int i = 0;
+        while (i < help.Length)
+        {
+            Console.WriteLine(help[i]);
+            i++;
+        }
+    }
+    else if (command == "SetName")
+    {
+        Console.Write("Введите Ваше имя: ");
+        name = Console.ReadLine();
+    }
+    else if (command == "SetPassword")
+    {
+        Console.Write("Введите пароль: ");
+        password = Console.ReadLine();
+    }
+    else if (command == "ResetPassword")
+    {
+        password = "";
+    }
+    else if (command == "ShowPassword")
+    {
+        Console.WriteLine(password);
+    }
+    else if (command == "WriteName")
+    {
+        Console.Write("Для того чтобы увидеть Имя нужно ввести пароль: ");
+        string passwordUser = Console.ReadLine();
+        if (passwordUser == password)
+        {
+            Console.WriteLine(name);
+        }
+        else
+        {
+            Console.WriteLine("Пароль не верен!");
+        }
+
+    }
+    else if (command.ToLower() == "exit")
+    {
         break;
     }
     else
     {
-        Console.WriteLine("Пароль не подходит!");
+        Console.WriteLine("Cписок доступных команд - введите \"Help\"");
     }
-count--;
-}*/
+}
